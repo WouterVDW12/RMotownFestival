67fd8e03e6cbc6f25978a5efbeaf5b0e6d378f82
Wouter Vandewalle <Wouter.Vandewalle@realdolmen.com>
2021-03-02 16:51:49 +0100

    sql database added

---
+++ backend/src/RMotownFestival.Api/DAL/MotownDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RMotownFestival.Api.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RMotownFestival.Api.DAL
+{
+    public class MotownDbContext : DbContext
+    {
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Stage> Stages { get; set; }
+
+        public MotownDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
