b27fcdd38544015ab569e63a80e4082d4cb83328
WouterVDW12 <78215889+WouterVDW12@users.noreply.github.com>
2021-03-02 13:50:43 +0100

    Update environment.prod.ts

--- frontend/RMotownFestival/src/environments/environment.prod.ts
+++ frontend/RMotownFestival/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   festivalName: "RMotownFestival",
-  apiBaseUrl: "https://rmotownfestivalapivdw.azurewebsites.net/api/"
+  apiBaseUrl: "https://????.azurewebsites.net/api/"
 };
